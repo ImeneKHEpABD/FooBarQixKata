bda4b901748748cb9843d0ed7ddac839e9eeaa38
Imene Khemiri <khemiriimene@gmail.com>
2018-12-17 23:20:25 +0100

    Add the test scenario with test result KO: should_return_string_that_contains_respectively_Foo_Bar_or_Qix_when_input_is_divisible_by_3_5_or_7

--- FooBarQixToolkit/FooBarQixToolkit.Tests/FooBarQixToolkitTests.cs
+++ FooBarQixToolkit/FooBarQixToolkit.Tests/FooBarQixToolkitTests.cs
@@ -17,5 +17,97 @@ namespace FooBarQixToolkit.Tests
             var computed = new FooBarQix().Compute(number);
             Assert.That(computed, Is.EqualTo(expected));
         }
+
+        [TestCase("9", "Foo")]
+        [TestCase("96", "Foo")]
+        [TestCase("24", "Foo")]
+        public void should_return_string_that_contains_only_one_Foo_when_input_is_only_divisible_by_3(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("13", "Foo")]
+        [TestCase("32", "Foo")]
+        public void should_return_string_that_contains_only_one_Foo_when_input_only_contains_only_3(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("3", "FooFoo")]
+        [TestCase("33", "FooFooFoo")]
+
+        public void should_return_string_that_contains_multiple_Foo_when_input_contains_at_least_one_3_and_is_divisisble_by_3(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("10", "Bar")]
+        public void should_return_string_that_contains_only_one_Bar_when_input_is_only_divisible_by_5(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("5", "BarBar")]
+        [TestCase("25", "BarBar")]
+        public void should_return_string_that_contains_multiple_Bar_when_input_string_is_divisible_by_5_and_contains_5(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("28", "Qix")]
+        public void should_return_string_that_contains_only_one_Qix_when_input_string_is_only_divisible_by_7(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("17", "Qix")]
+        [TestCase("47", "Qix")]
+        public void should_return_string_that_contains_only_one_Qix_when_input_string_contains_only_one_7(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("7", "QixQix")]
+        [TestCase("77", "QixQixQix")]
+        public void should_return_string_that_contains_multiple_Qix_when_input_is_divisible_by_7_and_contains_7(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("53", "BarFoo")]
+        public void should_return_string_that_contains_respectively_Bar_and_Foo_when_input_contains_respectively_5_and_3(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+        [TestCase("21", "FooQix")]
+        [TestCase("51", "FooBar")]
+        [TestCase("15", "FooBarBar")]
+        public void should_return_string_that_contains_respectively_Foo_Bar_or_Qix_when_input_is_divisible_by_3_5_or_7(string number, string expected)
+        {
+            var computed = new FooBarQix().Compute(number);
+            Assert.That(computed, Is.EqualTo(expected));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- FooBarQixToolkit/FooBarQixToolkit/FooBarQix.cs
+++ FooBarQixToolkit/FooBarQixToolkit/FooBarQix.cs
@@ -8,9 +8,102 @@ namespace FooBarQixToolkit
 {
     public class FooBarQix
     {
-        public string Compute(string strNumber)
+
+        public string Compute(string number)
         {
-            return string.Empty;
+            long integer;
+            var result = string.Empty;
+            try
+            {
+                if (!Int64.TryParse(number, out integer))
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    result = ApplytheDeviderRule(integer);
+                    result += ApplytheContainsRule(number);
+
+                    if (string.IsNullOrEmpty(result))
+                    {
+                        result = number.ToString();
+                    }
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public string ApplytheContainsRule(string number)
+        {
+
+            StringBuilder result = new StringBuilder();
+            try
+            {
+
+                if (!number.Contains("3")
+                    && !number.Contains("5")
+                    && !number.Contains("7"))
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    foreach(char c in number)
+                    {
+                        if (c == '3')
+                            result.Append("Foo");
+                        if (c == '5')
+                            result.Append("Bar");
+                        if (c == '7')
+                            result.Append("Qix");
+                    }
+                    return result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public string ApplytheDeviderRule(long number)
+        {
+            string result = string.Empty;
+            
+            try
+            {
+                if (number % 3 != 0
+                       && number % 5 != 0
+                       && number % 7 != 0)
+                {
+                    return string.Empty;
+                }
+                else
+                {
+                    if (number % 3 == 0)
+                    {
+                        result += "Foo";
+                    }
+                    if (number % 5 == 0)
+                    {
+                        result = "Bar";
+                    }
+                    if (number % 7 == 0)
+                    {
+                        result = "Qix";
+                    }
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
         }
     }
 }
