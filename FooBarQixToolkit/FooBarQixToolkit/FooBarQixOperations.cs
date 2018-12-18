051e340914533d731aed4dfbcce839041ee735ac
Imene Khemiri <khemiriimene@gmail.com>
2018-12-18 02:20:52 +0100

    Refactoring: Move the DicDividerRules dictionary and the ApplytheDeviderRule method from the FooBarQixOperations class to a separate class called FooBarQixRuleDividers

--- FooBarQixToolkit/FooBarQixToolkit/FooBarQixOperations.cs
+++ FooBarQixToolkit/FooBarQixToolkit/FooBarQixOperations.cs
@@ -10,17 +10,13 @@ namespace FooBarQixToolkit
     public class FooBarQixOperations
     {
         public FooBarQixRuleContains foobarqixrulecontains;
+        public FooBarQixRuleDividers foobarqixruledividers;
         private Logger logger = NLog.LogManager.GetCurrentClassLogger();
-        public Dictionary<int, string> DicDividerRules = new Dictionary<int, string>
-        {
-            [3] = "Foo",
-            [5] = "Bar",
-            [7] = "Qix"
-        };
-
+    
         public FooBarQixOperations()
         {
             foobarqixrulecontains = new FooBarQixRuleContains();
+            foobarqixruledividers = new FooBarQixRuleDividers();
         }
         public string EvaluateRules(string inputString)
         {
@@ -30,7 +26,7 @@ namespace FooBarQixToolkit
             {
                 if (Int64.TryParse(inputString, out parsedNumber))
                 {
-                    result = ApplytheDeviderRule(parsedNumber);
+                    result = foobarqixruledividers.ApplytheDeviderRule(parsedNumber);
 
                     result += BuildString(result, inputString);
                     if (string.IsNullOrEmpty(result))
@@ -46,28 +42,6 @@ namespace FooBarQixToolkit
                 logger.Error("EvaluateRules Error: " + ex.Message);
             }
             return result;
-        }
-
-        
-
-        public string ApplytheDeviderRule(long number)
-        {
-            string result = string.Empty;
-
-            try
-            {
-                foreach (var val in DicDividerRules)
-                {
-                    if (number % val.Key == 0)
-                        result += val.Value;
-                }
-                return result;
-            }
-            catch (Exception ex)
-            {
-                logger.Error("An error occurred in the ApplythedeviderRule method: " + ex.Message);
-                return string.Empty;
-            }
         }
 
         public string BuildString(string dividerresult, string number)
---
+++ FooBarQixToolkit/FooBarQixToolkit/FooBarQixRuleDividers.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBarQixToolkit
+{
+    public class FooBarQixRuleDividers
+    {
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        public Dictionary<int, string> DicDividerRules = new Dictionary<int, string>
+        {
+            [3] = "Foo",
+            [5] = "Bar",
+            [7] = "Qix"
+        };
+
+        public string ApplytheDeviderRule(long number)
+        {
+            string result = string.Empty;
+
+            try
+            {
+                foreach (var val in DicDividerRules)
+                {
+                    if (number % val.Key == 0)
+                        result += val.Value;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                logger.Error("An error occurred in the ApplythedeviderRule method: " + ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+}
